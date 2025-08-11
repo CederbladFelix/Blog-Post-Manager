527b03f14ac96c851942d6ae1ecc6bb082b2f28e
Felix Cederblad <felix_cederblad@hotmail.com>
2025-08-11 15:36:25 +0200

    blogpost, eventlistener

---
+++ blog-post-manager/src/blogpost.ts
@@ -0,0 +1,23 @@
+export interface BlogPost {
+  id: string;
+  title: string;
+  body: string;
+  author: string;
+  createdAt: Date;
+}
+
+let nextId = 1;
+
+export function createBlogPost(
+  title: string,
+  body: string,
+  author: string
+): BlogPost {
+  return {
+    id: (nextId++).toString(),
+    title,
+    body,
+    author,
+    createdAt: new Date(),
+  };
+}
--- blog-post-manager/src/main.ts
+++ blog-post-manager/src/main.ts
@@ -1,26 +1,47 @@
 import "./style.css";
+import type { BlogPost } from "./blogpost";
+import { createBlogPost } from "./blogpost";
+
+const posts: BlogPost[] = [];
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <div class="form-container">
     <form class="form">
       <div class="row">
         <label>Title</label>
-        <input></input>
+        <input type="text" name="title"></input>
       </div>
       <div class="row">
         <label>Body</label>
-        <textarea></textarea>
+        <textarea name="body"></textarea>
       </div>
       <div class="row">
         <label>Author</label>
-        <input></input>
+        <input type="text" name="author"></input>
       </div>
       <div>
-        <button class="submit">Submit</button>
+        <button type="submit" class="submit">Submit</button>
       </div>
     </form>
   </div>
-  <div>
-    <p>Blogposts here</p>
+  <div id="blogposts">
+
   </div>
 `;
+
+const form = document.querySelector<HTMLFormElement>(".form")!;
+const postsContainer = document.querySelector<HTMLDivElement>("#blogposts")!;
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const formdata = new FormData(form);
+
+  const title = formdata.get("title") as string;
+  const body = formdata.get("body") as string;
+  const author = formdata.get("author") as string;
+
+  const post = createBlogPost(title, body, author);
+  posts.unshift(post);
+  console.log(posts);
+});
